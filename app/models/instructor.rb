b5e3e52c0a93108034de997e70f50418e00f0771
kennja05 <jacobkenny05@gmail.com>
2019-12-16 14:28:43 -0500

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,39 @@
 class Instructor
 
+    attr_reader :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end 
+
+    def self.all
+        @@all
+    end 
+
+    def pass_student(student, test_name)
+        if student.tests.find {|test| test.test_name == test_name} == nil 
+            BoatingTest.new(student, test_name, self, "passed")
+        else 
+            student.tests.each do |test|
+                if test.test_name == test_name 
+                test.status = "passed"
+                end
+            end 
+        end 
+    end   
+    
+    def fail_student(student, test_name)
+        if student.tests.find {|test| test.test_name == test_name} == nil 
+            BoatingTest.new(student, test_name, self, "failed")
+        else    
+            student.tests.each do |test|
+                if test.test_name == test_name
+                test.status = "failed"
+                end 
+            end
+        end 
+    end 
+
 end
